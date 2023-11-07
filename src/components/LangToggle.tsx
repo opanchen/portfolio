08129022f7250a619e0c600cd806f70e2376f2d5
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2023-11-07 10:26:35 +0100

    updated lang & theme toggle components

--- src/components/LangToggle.tsx
+++ src/components/LangToggle.tsx
@@ -1,26 +1,82 @@
 import { useLocale } from "next-intl";
 
-import Link from "next-intl/link";
+import Image from "next/image";
+import Link from "next/link";
 
 const LangToggle: React.FC = () => {
   const currentLocale = useLocale();
 
   const getLinkClassNames = (locale: "en" | "uk") => {
-    return `p-[4px] text-[14px] ${
-      currentLocale === locale && "border-b-[2px] border-orange-500"
+    return `text-[14px] focus:outline-none group ${
+      currentLocale === locale && "pointer-events-none"
     }`;
+  };
+
+  const getLabel = (locale: "en" | "uk") => {
+    if (locale === "en") {
+      return currentLocale === "en"
+        ? "Switch language of interface to English"
+        : "Переключити інтерфейс на Англійську";
+    } else {
+      return currentLocale === "en"
+        ? "Switch language of interface to Ukrainian"
+        : "Переключити мову інтерфейсу на Українську";
+    }
   };
 
   return (
     <ul className="flex gap-[8px]">
       <li>
-        <Link href="/" locale="en" className={getLinkClassNames("en")}>
-          En
+        <Link
+          href="/en/"
+          className={getLinkClassNames("en")}
+          aria-label={getLabel("en")}
+        >
+          <div className="w-[32px] h-[20px] flex justify-center items-center overflow-hidden group-hover:scale-110 group-focus:scale-125 transition_prop relative">
+            <Image
+              src={
+                "http://purecatamphetamine.github.io/country-flag-icons/3x2/GB.svg"
+              }
+              width={36}
+              height={24}
+              alt={
+                currentLocale === "en" ? "British flag" : "Британський прапор"
+              }
+              className="block cover"
+            />
+            <div
+              className={`absolute top-0 bottom-0 left-0 w-full h-full bg-[#181818] bg-opacity-[0.7] ${
+                currentLocale === "en" ? "block" : " hidden"
+              }`}
+            ></div>
+          </div>
         </Link>
       </li>
+
       <li>
-        <Link href="/" locale="uk" className={getLinkClassNames("uk")}>
-          Uk
+        <Link
+          href="/uk/"
+          className={getLinkClassNames("uk")}
+          aria-label={getLabel("uk")}
+        >
+          <div className="w-[32px] h-[20px] flex justify-center items-center overflow-hidden group-hover:scale-110 group-focus:scale-125 transition_prop relative">
+            <Image
+              src={
+                "http://purecatamphetamine.github.io/country-flag-icons/3x2/UA.svg"
+              }
+              width={36}
+              height={24}
+              alt={
+                currentLocale === "en" ? "Ukrainian flag" : "Український прапор"
+              }
+              className="block cover"
+            />
+            <div
+              className={`absolute top-0 bottom-0 left-0 w-full h-full bg-[#181818] bg-opacity-[0.7] ${
+                currentLocale === "uk" ? "block" : "hidden"
+              }`}
+            ></div>
+          </div>
         </Link>
       </li>
     </ul>
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { useState, useEffect } from "react";
+
 import { useTheme } from "next-themes";
+import { useLocale } from "next-intl";
+
+import { FaMoon } from "react-icons/fa";
+import { BsSunFill } from "react-icons/bs";
 
 const ThemeToggle: React.FC = () => {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  const locale = useLocale();
 
   useEffect(() => {
     setMounted(true);
@@ -15,33 +22,28 @@ const ThemeToggle: React.FC = () => {
     return null;
   }
 
-  const getBtnClassNames = (mode: "dark" | "light") => {
-    return `p-[4px] text-[14px] ${
-      theme === mode && "border-b-[2px] border-orange-500"
-    }`;
+  const toggleTheme = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
   };
 
+  const label = locale === "en" ? "Toggle theme mode" : "Перемкнути режим теми";
+
   return (
-    <ul className="flex gap-[8px]">
-      <li>
-        <button
-          type="button"
-          className={getBtnClassNames("light")}
-          onClick={() => setTheme("light")}
-        >
-          Light
-        </button>
-      </li>
-      <li>
-        <button
-          type="button"
-          className={getBtnClassNames("dark")}
-          onClick={() => setTheme("dark")}
-        >
-          Dark
-        </button>
-      </li>
-    </ul>
+    <>
+      <button
+        className="shadow-sm hover:shadow hover:scale-110 focus:outline-none    focus:shadow focus:scale-110 transition_prop dark:shadow-zinc-100 relative w-[56px] h-[24px] p-[4px] flex items-center cursor-pointer bg-sky-200 dark:bg-gray-900 rounded-full"
+        onClick={toggleTheme}
+        type="button"
+        aria-label={label}
+      >
+        <FaMoon className={"text-white"} size={16} />
+        <div
+          className="absolute bg-white w-[20px] h-[20px] rounded-full shadow-md transform transition-transform duration-300"
+          style={theme === "dark" ? { left: "2px" } : { right: "2px" }}
+        ></div>
+        <BsSunFill className={"ml-auto text-yellow-400"} size={16} />
+      </button>
+    </>
   );
 };
 
